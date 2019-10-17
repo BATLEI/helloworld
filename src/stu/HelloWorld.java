e85ea297ec200490ecc08a8dcafccba1c6a61be5
BATLEI <xiaolei9606@163.com>
2019-10-17 14:40:54 +0800

    111

---
+++ src/stu/HelloWorld.java
@@ -0,0 +1,18 @@
+package stu;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Map<Object, Object> map = new HashMap<>();
+        map.put("a","aa");
+        map.put("b","bb");
+        map.put("c","cc");
+        System.out.println(map);
+        for (Map.Entry<Object, Object> entry : map.entrySet()) {
+            System.out.println(entry.getValue());
+        }
+
+    }
+}
